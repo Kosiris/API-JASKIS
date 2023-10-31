460854faae8805cdaf6ec01ad378b2582241390f
Cam <farrcameron@gmail.com>
2023-10-30 19:47:44 -0700

    Update commands.js

--- commands.js
+++ commands.js
@@ -88,13 +88,13 @@ db.bounties.insertMany(
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
-
+db.bounties.find({location: "Grasslands"})
 // 2. Query for all bounties with a reward worth 10000 or more
-
+db.bounties.find({reward:{$gt: 10000}})
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.find({}, { client: 0 })
 // 4. Query for a Groundhog in the Woodlands
-
+db.bounties.find({ $and: [ { species: "Groundhog" }, { location: "Woodlands"} ] })
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
 db.bounties.updateOne({name: "Polarwind"},
